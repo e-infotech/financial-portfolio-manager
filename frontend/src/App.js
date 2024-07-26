31d40a531bb91989751a0d3b3b71b3e1f962a67f
e-infotech <45136020+e-infotech@users.noreply.github.com>
2024-07-26 19:39:01 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import components here
+import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-          {/* Add routes here */}
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          {/* Add more routes here */}
         </Switch>
       </div>
     </Router>
