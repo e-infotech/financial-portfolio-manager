8f228321532e83ba50dd73808ef5d6b980bc02d8
e-infotech <45136020+e-infotech@users.noreply.github.com>
2024-07-26 19:31:49 -0400

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import components here
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          {/* Add routes here */}
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
