69b7539410bb301bc245da3828a636724e833034
e-infotech <45136020+e-infotech@users.noreply.github.com>
2024-07-28 14:53:49 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,20 @@ import Register from './components/Register';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import Portfolio from './components/Portfolio';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/portfolio" component={Portfolio} />
-          {/* Add more routes here */}
-        </Switch>
+        <div className="container">
+          <Switch>
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/portfolio" component={Portfolio} />
+          </Switch>
+        </div>
       </div>
     </Router>
   );
